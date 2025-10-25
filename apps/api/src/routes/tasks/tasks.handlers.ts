177bf27e0f79536e80fe8a3207c5f850d904bd70
Mika Vallittu <mika.vallittu@gmail.com>
2025-10-25 21:34:40 +0300

    feat: implement task CRUD operations and update routing in tasks index

--- apps/api/src/routes/tasks/tasks.handlers.ts
+++ apps/api/src/routes/tasks/tasks.handlers.ts
@@ -1,19 +1,105 @@
-export async function listTasks(c) {
-  return "list tasks";
-}
+import * as HttpStatusCodes from "stoker/http-status-codes";
+import * as HttpStatusPhrases from "stoker/http-status-phrases";
+import * as z from "zod";
+import { createFactory } from "hono/factory";
+import { zValidator } from "@hono/zod-validator";
 
-export async function createTask(c) {
-  return "create a task";
-}
+const factory = createFactory();
 
-export async function getTask(c) {
-  return `get ${id}`;
-}
+const taskSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  completed: z.boolean(),
+});
 
-export async function updateTask(c) {
-  return `update ${id}`;
-}
+const IdParamsSchema = z.object({
+  id: z.coerce.number(),
+});
 
-export async function deleteTask(c) {
-  return `delete ${id}`;
-}
+// type Task = z.infer<typeof taskSchema>;
+
+const tasks = [
+  { id: 1, title: "Task 1", completed: false },
+  { id: 2, title: "Task 2", completed: true },
+  { id: 3, title: "Task 3", completed: false },
+];
+
+const list = factory.createHandlers(
+  function listTasks(c) {
+    return c.json(tasks);
+  },
+);
+
+const create = factory.createHandlers(
+  zValidator("json", taskSchema),
+  function createTask(c) {
+    const task = c.req.valid("json");
+    tasks.push(task);
+    return c.json(task, HttpStatusCodes.OK);
+  },
+);
+
+const getOne = factory.createHandlers(
+  zValidator("param", IdParamsSchema),
+  function getTask(c) {
+    const { id } = c.req.valid("param");
+    const task = tasks.find((t) => t.id === id);
+
+    if (!task) {
+      return c.json(
+        {
+          message: HttpStatusPhrases.NOT_FOUND,
+        },
+        HttpStatusCodes.NOT_FOUND,
+      );
+    }
+
+    return c.json(task, HttpStatusCodes.OK);
+  },
+);
+
+const update = factory.createHandlers(
+  zValidator("param", IdParamsSchema),
+  zValidator("json", taskSchema.omit({ id: true })),
+  function updateTask(c) {
+    const { id } = c.req.valid("param");
+    const task = c.req.valid("json");
+    const index = tasks.findIndex((t) => t.id === id);
+
+    if (index === -1) {
+      return c.json(
+        {
+          message: HttpStatusPhrases.NOT_FOUND,
+        },
+        HttpStatusCodes.NOT_FOUND,
+      );
+    }
+
+    tasks[index] = { id, ...task };
+
+    return c.json(task, HttpStatusCodes.OK);
+  },
+);
+
+const remove = factory.createHandlers(
+  zValidator("param", IdParamsSchema),
+  function deleteTask(c) {
+    const { id } = c.req.valid("param");
+    const index = tasks.findIndex((t) => t.id === id);
+
+    if (index === -1) {
+      return c.json(
+        {
+          message: HttpStatusPhrases.NOT_FOUND,
+        },
+        HttpStatusCodes.NOT_FOUND,
+      );
+    }
+
+    tasks.splice(index, 1);
+
+    return c.body(null, HttpStatusCodes.NO_CONTENT);
+  },
+);
+
+export { create, getOne, list, remove, update };
--- apps/api/src/routes/tasks/tasks.index.ts
+++ apps/api/src/routes/tasks/tasks.index.ts
@@ -1,45 +1,12 @@
 import createApp from "../../lib/create-app.ts";
-import * as Status from "stoker/http-status-codes";
-import * as z from "zod";
-import { zValidator } from "@hono/zod-validator";
-import { createFactory } from "hono/factory";
-
-const taskSchema = z.object({
-  id: z.number(),
-  title: z.string(),
-  completed: z.boolean(),
-});
-
-// type Task = z.infer<typeof taskSchema>;
-
-const tasks = [
-  { id: 1, title: "Task 1", completed: false },
-  { id: 2, title: "Task 2", completed: true },
-  { id: 3, title: "Task 3", completed: false },
-];
-
-const factory = createFactory();
-
-const createTaskHandlers = factory.createHandlers(
-  zValidator("json", taskSchema),
-  function createTask(c) {
-    const task = c.req.valid("json");
-    tasks.push(task);
-    return c.json(task, Status.OK);
-  },
-);
-
-const listTasksHandlers = factory.createHandlers(function listTasks(c) {
-  return c.json(tasks);
-});
+import { create, getOne, list, remove, update } from "./tasks.handlers.ts";
 
 const app = createApp("/tasks")
-  .get("/", ...listTasksHandlers)
-  .post("/", ...createTaskHandlers);
-
-// .get("/:id", getTask)
-// .patch("/:id", updateTask)
-// .delete("/:id", deleteTask);
+  .get("/", ...list)
+  .post("/", ...create)
+  .get("/:id", ...getOne)
+  .patch("/:id", ...update)
+  .delete("/:id", ...remove);
 
 export default app;
 export type AppType = typeof app;
